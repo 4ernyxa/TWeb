c573cd9a4c3c53e3b0ce31b184a42d4c5945ada5
Vladimir <noobgithacker@gmail.com>
2022-04-16 01:04:48 +0300

    crearea interfatei

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,10 +10,51 @@ function App() {
         <p>
           Ursatii Vladimir Cr-193
         </p>
-
+          <InterfaceFive></InterfaceFive>
+          <InterfaceSeven></InterfaceSeven>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function InputField() {
+    const [input, setInput] = useState('');
+    console.log(input);
+    return (
+
+        <div style={{
+                width: 500,
+                maxWidth: '100%',
+            }}
+        >
+            <input value={input} onInput={e => setInput((e.target as HTMLTextAreaElement).value)}/>
+            <span>{input}</span>
+        </div>
+    );
+}
+
+function InterfaceFive() {
+    return (
+        <div>
+            <InputField></InputField>
+            <InputField></InputField>
+            <InputField></InputField>
+            <InputField></InputField>
+            <InputField></InputField>
+            <h1>End interface5 fields</h1>
+        </div>
+    );
+}
+
+function InterfaceSeven() {
+    return (
+        <div>
+            <InterfaceFive></InterfaceFive>
+            <InputField></InputField>
+            <InputField></InputField>
+            <h1>End interface7 fields</h1>
+        </div>
+    );
+}
